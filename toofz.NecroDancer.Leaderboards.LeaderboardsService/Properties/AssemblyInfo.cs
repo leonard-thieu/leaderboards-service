33ad20ce3e80fcd33edaffc6497a003857e11e08
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-09 14:28:30 -0400

    Bump version.

--- toofz.NecroDancer.Leaderboards.LeaderboardsService/Properties/AssemblyInfo.cs
+++ toofz.NecroDancer.Leaderboards.LeaderboardsService/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("toofz Leaderboards Service")]
 [assembly: AssemblyCopyright("Copyright © Leonard Thieu 2015")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("2.0.*")]
 
 [assembly: InternalsVisibleTo("toofz.NecroDancer.Leaderboards.LeaderboardsService.Tests")]
 
