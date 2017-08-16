22302a2727f8a07d462fb80b02f87dfc2b204095
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-16 16:35:27 -0400

    Use shorter solution and project names.

--- LeaderboardsService/Properties/AssemblyInfo.cs
+++ LeaderboardsService/Properties/AssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("2.1.0.*")]
 
-[assembly: InternalsVisibleTo("toofz.NecroDancer.Leaderboards.LeaderboardsService.Tests")]
+[assembly: InternalsVisibleTo("LeaderboardsService.Tests")]
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true, ConfigFile = "log.config")]
