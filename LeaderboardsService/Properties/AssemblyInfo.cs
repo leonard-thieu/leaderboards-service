8ac99cbc3e8b3a74925268f894971a131bb83886
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-17 21:27:14 -0400

    v2.1.2

--- LeaderboardsService/Properties/AssemblyInfo.cs
+++ LeaderboardsService/Properties/AssemblyInfo.cs
@@ -2,13 +2,14 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyVersion("2.1.2.0")]
+
+[assembly: AssemblyCopyright("Copyright © Leonard Thieu 2017")]
+[assembly: AssemblyProduct("toofz")]
+
 [assembly: AssemblyTitle("toofz Leaderboards Service")]
-[assembly: AssemblyDescription("Provides Steam leaderboard data.")]
-[assembly: AssemblyCompany("toofz")]
-[assembly: AssemblyProduct("toofz Leaderboards Service")]
-[assembly: AssemblyCopyright("Copyright © Leonard Thieu 2015")]
+
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.1.1.*")]
 
 [assembly: InternalsVisibleTo("LeaderboardsService.Tests")]
 
