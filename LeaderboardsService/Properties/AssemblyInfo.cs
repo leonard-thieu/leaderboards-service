c72e39b6b349b9a15b1e6a643ab5561303b188e8
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-13 16:03:55 -0400

    v4.1.4

--- LeaderboardsService/Properties/AssemblyInfo.cs
+++ LeaderboardsService/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using log4net.Config;
 
-[assembly: AssemblyVersion("4.1.3.0")]
+[assembly: AssemblyVersion("4.1.4.0")]
 
 [assembly: AssemblyCopyright("Copyright © Leonard Thieu 2017")]
 [assembly: AssemblyProduct("toofz")]
