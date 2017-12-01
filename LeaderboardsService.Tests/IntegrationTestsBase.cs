0933594ed3d2b7eb43e6b4f78b655dd6502508c9
Leonard Thieu <leonard.thieu@gmail.com>
2017-12-01 14:26:46 -0500

    Clean up tests.

---
+++ LeaderboardsService.Tests/IntegrationTestsBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Configuration;
+using System.Data.Entity;
+using System.IO;
+using toofz.NecroDancer.Leaderboards.LeaderboardsService.Properties;
+using toofz.Services;
+using Xunit;
+
+namespace toofz.NecroDancer.Leaderboards.LeaderboardsService.Tests
+{
+    [Trait("Category", "Uses SQL Server")]
+    [Trait("Category", "Uses file system")]
+    [Collection("Uses SQL Server and file system")]
+    public abstract class IntegrationTestsBase : IDisposable
+    {
+        public IntegrationTestsBase()
+        {
+            settings = Settings.Default;
+            // Should only loop once
+            foreach (SettingsProvider provider in settings.Providers)
+            {
+                var ssp = (ServiceSettingsProvider)provider;
+                ssp.GetSettingsReader = () => File.OpenText(settingsFileName);
+                ssp.GetSettingsWriter = () => File.CreateText(settingsFileName);
+            }
+
+            db = new LeaderboardsContext(databaseConnectionString);
+            db.Database.Delete(); // Make sure it really dropped - needed for dirty database
+            Database.SetInitializer(new LeaderboardsContextInitializer());
+            db.Database.Initialize(force: true);
+            Database.SetInitializer(new NullDatabaseInitializer<LeaderboardsContext>());
+        }
+
+        internal readonly Settings settings;
+        private readonly string settingsFileName = Path.GetTempFileName();
+        protected readonly string databaseConnectionString = StorageHelper.GetDatabaseConnectionString();
+        protected readonly LeaderboardsContext db;
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (File.Exists(settingsFileName)) { File.Delete(settingsFileName); }
+                db.Database.Delete();
+            }
+        }
+    }
+}
--- LeaderboardsService.Tests/WorkerRoleTests.cs
+++ LeaderboardsService.Tests/WorkerRoleTests.cs
@@ -1,59 +1,34 @@
 ﻿using System;
-using System.Configuration;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using log4net;
 using Microsoft.ApplicationInsights;
 using Moq;
 using Ninject.Extensions.NamedScope;
-using toofz.NecroDancer.Leaderboards.LeaderboardsService.Properties;
 using toofz.NecroDancer.Leaderboards.Steam.ClientApi;
 using toofz.NecroDancer.Leaderboards.Steam.CommunityData;
-using toofz.Services;
 using Xunit;
 
 namespace toofz.NecroDancer.Leaderboards.LeaderboardsService.Tests
 {
     public class WorkerRoleTests
     {
-        public class IntegrationTests : DatabaseTestsBase
+        public class IntegrationTests : IntegrationTestsBase
         {
-            private readonly string settingsFileName = Path.GetTempFileName();
             private readonly Mock<ILog> mockLog = new Mock<ILog>();
 
-            protected override void Dispose(bool disposing)
-            {
-                if (disposing)
-                {
-                    if (File.Exists(settingsFileName)) { File.Delete(settingsFileName); }
-                }
-
-                base.Dispose(disposing);
-            }
-
-            [Trait("Category", "Uses file system")]
             [Fact]
             public async Task ExecutesUpdateCycle()
             {
                 // Arrange
-                var settings = Settings.Default;
-                // Should only loop once
-                foreach (SettingsProvider provider in settings.Providers)
-                {
-                    var ssp = (ServiceSettingsProvider)provider;
-                    ssp.GetSettingsReader = () => File.OpenText(settingsFileName);
-                    ssp.GetSettingsWriter = () => File.CreateText(settingsFileName);
-                }
                 settings.UpdateInterval = TimeSpan.Zero;
                 var telemetryClient = new TelemetryClient();
                 var runOnce = true;
 
                 var kernel = KernelConfig.CreateKernel();
 
-                var connectionString = StorageHelper.GetDatabaseConnectionString();
                 kernel.Rebind<string>()
-                      .ToConstant(connectionString)
+                      .ToConstant(databaseConnectionString)
                       .WhenInjectedInto(typeof(LeaderboardsContext), typeof(LeaderboardsStoreClient));
 
                 kernel.Rebind<ILeaderboardsStoreClient>()
@@ -69,11 +44,13 @@ namespace toofz.NecroDancer.Leaderboards.LeaderboardsService.Tests
                       .InParentScope();
 
                 var log = mockLog.Object;
-                var worker = new WorkerRole(settings, telemetryClient, runOnce, kernel, log);
 
                 // Act
-                worker.Start();
-                await worker.Completion;
+                using (var worker = new WorkerRole(settings, telemetryClient, runOnce, kernel, log))
+                {
+                    worker.Start();
+                    await worker.Completion;
+                }
 
                 // Assert
                 Assert.True(db.Leaderboards.Any(l => l.LastUpdate != null));
